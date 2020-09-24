b97163e86f4d9489a20114e2e1ea37c5918dc73d
Yihui Liao <liao.yihui18@gmail.com>
2020-09-23 22:32:04 -0400

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Yihui Liao on 9/23/20.
+//  Copyright © 2020 Yihui Liao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

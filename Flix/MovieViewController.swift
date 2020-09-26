93d53a458638391ef1527028303eeb2452732f61
Yihui Liao <liao.yihui18@gmail.com>
2020-09-26 00:01:49 -0400

    use library to add images

--- Flix/MovieViewController.swift
+++ Flix/MovieViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -60,6 +61,12 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        
+        cell.posterView.af_setImage(withURL: posterURL!)
         
         return cell
     }

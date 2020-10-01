a134ec430c0e913f376768c8ab01dab2b0787773
Yihui Liao <liao.yihui18@gmail.com>
2020-09-30 23:26:47 -0400

    basic layout of movie details screen completed

--- Flix/MovieDetailsViewController.swift
+++ Flix/MovieDetailsViewController.swift
@@ -26,13 +26,21 @@ class MovieDetailsViewController: UIViewController {
         
         
         titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
         synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()   
         
         let baseURL = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
         let posterURL = URL(string: baseURL + posterPath)
         
         posterView.af_setImage(withURL: posterURL!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropURL = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropView.af_setImage(withURL: backdropURL!)
     }
     
 

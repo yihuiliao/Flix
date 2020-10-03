8b19d2c2f40114aa94a5412752dbfb50b8dc2e81
Yihui Liao <liao.yihui18@gmail.com>
2020-10-02 21:04:59 -0400

    added network request for superhero

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Yihui Liao on 10/2/20.
+//  Copyright © 2020 Yihui Liao. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
